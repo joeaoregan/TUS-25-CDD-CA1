addeba44d45f3f6a36710fd1c418276a8bc03e87
joeaoregan <joe.oregan@gmail.com>
2025-11-03 20:18:25 +0000

    user data, python load test

--- RestAPI/api_test.py
+++ RestAPI/api_test.py
@@ -2,12 +2,18 @@ import requests
 import time 
 
 # URL for REST API to test
-IPv4_ADDR = "13.218.220.21"
+# IPv4_ADDR = "13.218.220.21"
+# IPv4_ADDR = "ec2-98-80-7-17.compute-1.amazonaws.com"
+# IPv4_ADDR = "ec2-18-208-251-209.compute-1.amazonaws.com"
+# IPv4_ADDR = "ec2-3-91-76-75.compute-1.amazonaws.com"
+# IPv4_ADDR = "ec2-54-163-16-200.compute-1.amazonaws.com" # Fibonnaci v2
+# IPv4_ADDR = "ec2-13-221-17-181.compute-1.amazonaws.com" # Factorial v2
+IPv4_ADDR = "ec2-54-237-139-69.compute-1.amazonaws.com" # Book
 PORT = "9091"
 URL = f"http://{IPv4_ADDR}:{PORT}/couponapi/coupons"
 
 def main():
-    for i in range(1, 2501): # Make 100 requests
+    for i in range(1, 1001): # Make 1000 requests
         start = time.time()
         try:
             response = requests.get(URL, timeout=5) # GET request
