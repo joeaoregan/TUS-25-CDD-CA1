989245e033e2b597c440bd0ee6c5c727e1a0869a
joeaoregan <joe.oregan@gmail.com>
2025-11-03 20:23:54 +0000

    sql insert table data

---
+++ db/insert_data.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM COUPON;
+
+INSERT INTO servicedb.coupon VALUES (1, "ABC", 10.000, "30/10/2022");
+INSERT INTO servicedb.coupon VALUES (2, "SUMMERSALE", 10.000, "12/12/2020");
+
+SELECT * FROM servicedb.coupon;
